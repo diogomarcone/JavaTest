4dd4dd8f8c62842d2ea79880dc1868cbc089d2c2
Diogo Marcone <diogo.marcone@hotmail.com>
2022-02-04 10:57:50 -0300

    Inicializado projeto

---
+++ src/main/java/com/javatest/SigaBemvalorFrete/SigaBemValorFreteApplication.java
@@ -0,0 +1,13 @@
+package com.javatest.SigaBemvalorFrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigaBemValorFreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigaBemValorFreteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatest/SigaBemvalorFrete/SigaBemValorFreteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatest.SigaBemvalorFrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigaBemValorFreteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
